8245f3041f7caf6f6fa588013890e3cdbcebe736
TsekhanDmytro <tsekhandmytro@gmail.com>
2022-07-27 18:46:16 +0300

    homework

---
+++ JavaScript/lesson2/script.js
@@ -0,0 +1,44 @@
+// // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let arr = ['dime', 15, true, 'assa', [], 586, false, {}, '     ', 'qweqweqe']
+// for (const arrElement of arr) {
+//     console.log(arrElement);
+// }
+
+// // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let book = {
+//     title: 'Робіндзон',
+//     pageCount: 522,
+//     genre: 'Пригоди'
+// };
+// console.log(book.title);
+
+
+// // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+//  let book = {
+//      title: 'Робіндзон',
+//      pageCount: 522,
+//      genre: 'Пригоди',
+//      autors: [
+//          {name: 'asdasd', age:59 },
+//          {name: 'zxczxz', age:26}
+//      ]
+//  };
+//  console.log(book.autors[0].age);
+
+// // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+// let users =[
+//     {name: 'Masha', userName: 'Ivan', password: 111},
+//     {name: 'Anna', userName: 'Masha', password: 222},
+//     {name: 'Filya', userName: 'Anna', password: 333},
+//     {name: 'Olaf', userName: 'Filya', password: 444},
+//     {name: 'Andre', userName: 'Olaf', password: 555},
+//     {name: 'Dima', userName: 'Andre', password: 'aaa'},
+//     {name: 'Vasya', userName: 'Dima', password: 'bbb'},
+//     {name: 'Sveta', userName: 'Vasya', password: 'ccc'},
+//     {name: 'Taras', userName: 'Sveta', password: 'ddd'},
+//     {name: 'Ivan', userName: 'Anton', password: 'eee'}
+// ];
+// for (const user of users) {
+//     console.log(user.password)
+// }
+
