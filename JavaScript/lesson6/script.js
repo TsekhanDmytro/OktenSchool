b6495667654f755bbcebcab5222d14c0036e39ba
TsekhanDmytro <tsekhandmytro@gmail.com>
2022-08-24 17:08:35 +0300

    homework

--- JavaScript/lesson6/script.js
+++ JavaScript/lesson6/script.js
@@ -36,8 +36,10 @@
 
 // // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
 // let arr = [10,8,-7,55,987,-1011,0,1050,0]
-// let newArr= arr.map(value =>'value')
+// let newArr= arr.map(value =>value.toString())
 // console.log(newArr)
+
+
 
 // // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
 // //     let nums = [11,21,3];
@@ -52,9 +54,8 @@
 //     }
 // };
 // sortNums(nums, 'descending');
-//----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// - є масив
+// // - є масив
 // let coursesAndDurationArray = [
 //     {title: 'JavaScript Complex', monthDuration: 5},
 //     {title: 'Java Complex', monthDuration: 6},
@@ -63,29 +64,9 @@
 //     {title: 'FullStack', monthDuration: 7},
 //     {title: 'Frontend', monthDuration: 4}
 // ];
-// -- відсортувати його за спаданням за monthDuration
-// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
-//
-// описати колоду карт
-// - знайти піковий туз
-// - всі шістки
-// - всі червоні карти
-// - всі буби
-// - всі трефи від 9 та більше
-//
-//
-// {
-//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
-//         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
-//     color:'', // 'red','black'
-// }
-//
-//
-// Додатково по reduce
-// Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
-// {
-//     spades:[],
-//         diamonds:[],
-//     hearts:[],
-//     clubs:[]
-// }
+// // -- відсортувати його за спаданням за monthDuration
+// console.log(coursesAndDurationArray.sort((a, b) => a.monthDuration - b.monthDuration));
+// // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// let newCoursesAndDurationArray =(coursesAndDurationArray.filter(value =>value.monthDuration>=5))
+// console.log(newCoursesAndDurationArray);
+
